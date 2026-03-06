3d44060aff5011b7948e088cd1903f333e5e2bbf
KhalidOwlWalid <khalidowlwalid@gmail.com>
2026-03-06 19:54:54 +0000

    [refactor] Move everything into PropellerInfo class

--- propellers/prop_modeling.py
+++ propellers/prop_modeling.py
@@ -6,60 +6,57 @@ filename = "datasets/PER3_10x45MR.dat"
 
 class PropellerInfo():
 
-    def __init__(self):
+    def __init__(self, dat_file):
+        self._dat_filepath = dat_file
         self._prop_info = dict()
+
+        self._df = open(self._dat_filepath, "r")
+        
+        self._df = self._df.read().splitlines()
+
+        for i, data in enumerate(self._df):
+            if data.isspace():
+                del self._df[i]
+
+        self._df = np.array(self._df)
+
+        for i, rpm_info_idx in enumerate(self._find_prop_header()):
+            # So far, the number of lines for each prop rpm dataset is 15
+            self._process_dataset(rpm_info_idx, 15)
 
     def request_data(self, prop_rpm: int, key: str):
         return self._prop_info[prop_rpm][key]
 
-with open(filename) as f:
+    def _find_prop_header(self):
+        prop_rpm_header_loc = np.strings.find(self._df, "PROP RPM")
+        prop_rpm_header_loc = np.where(prop_rpm_header_loc > 0, True, False)
+        prop_rpm_header_idx = np.nonzero(prop_rpm_header_loc)
+        return prop_rpm_header_idx[0]
 
-    def dat_file_cleanup(dataset):
+    def _process_dataset(self, start_idx: int, n_lines: int):
+        prop_rpm_segment = self._df[start_idx:start_idx + n_lines]
+        
+        # The output of the element that contains "PROP RPM" should look like the following
+        # PROP RPM = xxxx
+        # In this case, we care about the prop rpm, hence I am simply slicing the bit where it contains the actual
+        # rotor speed (e.g. 1000)
+        prop_rpm_header = int(prop_rpm_segment[0].split()[3])
 
-        for i, data in enumerate(dataset):
-            if data.isspace():
-                del dataset[i]
+        self._prop_info[prop_rpm_header] = {}
 
-    Propeller = PropellerInfo()
-
-    prop_perf_data = (f.read().splitlines())
-
-    dat_file_cleanup(prop_perf_data)
-    
-    # Convert the list into numpy array for easier handling
-    prop_perf_data = np.array(prop_perf_data)
-    prop_rpm_header_loc = np.strings.find(prop_perf_data, "PROP RPM")
-    prop_rpm_header_loc = np.where(prop_rpm_header_loc > 0, True, False)
-    
-    test_dataset = prop_perf_data[15:30]
-    prop_rpm_header = int(test_dataset[0].split()[3])
-
-    Propeller._prop_info[prop_rpm_header] = {}
-
-    for i, header_info in enumerate(test_dataset[1].split()):
-        Propeller._prop_info[prop_rpm_header][header_info] = np.array([])
+        for i, header_info in enumerate(prop_rpm_segment[1].split()):
+            self._prop_info[prop_rpm_header][header_info] = np.array([])
 
 
-    clean_dataset = list()
-    for curr_idx in range(3, len(test_dataset), 1):
-        clean_dataset.append(test_dataset[curr_idx].split())
+        clean_dataset = list()
+        for curr_idx in range(3, len(prop_rpm_segment), 1):
+            clean_dataset.append(prop_rpm_segment[curr_idx].split())
 
-    final_dataset = np.array(clean_dataset)
+        final_dataset = np.array(clean_dataset)
 
-    for i, header_info in enumerate(Propeller._prop_info[prop_rpm_header].keys()):
-        Propeller._prop_info[prop_rpm_header][header_info] = final_dataset[:, i]
+        for i, header_info in enumerate(self._prop_info[prop_rpm_header].keys()):
+            self._prop_info[prop_rpm_header][header_info] = final_dataset[:, i] 
 
-    print(Propeller._prop_info[prop_rpm_header].keys())
 
-    plt.plot(Propeller.request_data(prop_rpm_header, 'V'), Propeller.request_data(prop_rpm_header, 'Cp'))
-    plt.show()
-
-    # for i in range(30 - 18):
-    #     offset = 3
-    #     curr_idx = i + offset
-    #     print(test_dataset[curr_idx])
-        # test_dataset[curr_idx] = np.array(test_dataset[curr_idx].split()).T
-
-    # print(test_dataset)
-
-    # print(prop_rpm_header)
+if __name__ == "__main__":
+    data = PropellerInfo(filename)
